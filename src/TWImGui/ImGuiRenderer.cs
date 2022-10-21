136b2a91ea2d9c64a22b003d77f753ac3d1e3955
marpe <marpe@kensei.games>
2022-10-21 21:29:18 +0200

    WIP

--- src/TWImGui/ImGuiRenderer.cs
+++ src/TWImGui/ImGuiRenderer.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Text;
 using ImGuiNET;
 using SDL2;
 
@@ -39,6 +40,18 @@ public class ImGuiRenderer
     private readonly Sampler _textureSampler;
     private readonly GraphicsPipeline _pipeline;
 
+    private Platform_CreateWindow _createWindow;
+    private Platform_DestroyWindow _destroyWindow;
+    private Platform_GetWindowPos _getWindowPos;
+    private Platform_ShowWindow _showWindow;
+    private Platform_SetWindowPos _setWindowPos;
+    private Platform_SetWindowSize _setWindowSize;
+    private Platform_GetWindowSize _getWindowSize;
+    private Platform_SetWindowFocus _setWindowFocus;
+    private Platform_GetWindowFocus _getWindowFocus;
+    private Platform_GetWindowMinimized _getWindowMinimized;
+    private Platform_SetWindowTitle _setWindowTitle;
+
     public ImGuiRenderer(Game game)
     {
         _game = game;
@@ -71,6 +84,61 @@ public class ImGuiRenderer
         Inputs.TextInput += OnTextInput;
 
         BuildFontAtlas();
+
+        SetupMultiViewport(_game.MainWindow);
+    }
+
+    private unsafe void SetupMultiViewport(Window mainWindow)
+    {
+        var platformIO = ImGui.GetPlatformIO();
+        var mainViewport = ImGui.GetMainViewport();
+        mainViewport.PlatformHandle = mainWindow.Handle;
+        var gcHandle = GCHandle.Alloc(mainWindow);
+        mainViewport.PlatformUserData = (IntPtr)gcHandle;
+        SDL.SDL_SysWMinfo info = new();
+        SDL.SDL_VERSION(out info.version);
+        if (SDL.SDL_bool.SDL_TRUE == SDL.SDL_GetWindowWMInfo(mainWindow.Handle, ref info))
+        {
+            mainViewport.PlatformHandleRaw = info.info.win.window;
+        }
+
+        _createWindow = CreateWindow;
+        _destroyWindow = DestroyWindow;
+        _getWindowPos = GetWindowPos;
+        _setWindowPos = SetWindowPos;
+        _setWindowSize = SetWindowSize;
+        _getWindowSize = GetWindowSize;
+        _showWindow = ShowWindow;
+        _setWindowFocus = SetWindowFocus;
+        _getWindowFocus = GetWindowFocus;
+        _getWindowMinimized = GetWindowMinimized;
+        _setWindowTitle = SetWindowTitle;
+
+        platformIO.Platform_CreateWindow = Marshal.GetFunctionPointerForDelegate(_createWindow);
+        platformIO.Platform_DestroyWindow = Marshal.GetFunctionPointerForDelegate(_destroyWindow);
+        platformIO.Platform_ShowWindow = Marshal.GetFunctionPointerForDelegate(_showWindow);
+        platformIO.Platform_SetWindowPos = Marshal.GetFunctionPointerForDelegate(_setWindowPos);
+        platformIO.Platform_SetWindowSize = Marshal.GetFunctionPointerForDelegate(_setWindowSize);
+        platformIO.Platform_SetWindowFocus = Marshal.GetFunctionPointerForDelegate(_setWindowFocus);
+        platformIO.Platform_GetWindowFocus = Marshal.GetFunctionPointerForDelegate(_getWindowFocus);
+        platformIO.Platform_GetWindowMinimized = Marshal.GetFunctionPointerForDelegate(_getWindowMinimized);
+        platformIO.Platform_SetWindowTitle = Marshal.GetFunctionPointerForDelegate(_setWindowTitle);
+
+        ImGuiNative.ImGuiPlatformIO_Set_Platform_GetWindowPos(platformIO.NativePtr, Marshal.GetFunctionPointerForDelegate(_getWindowPos));
+        ImGuiNative.ImGuiPlatformIO_Set_Platform_GetWindowSize(platformIO.NativePtr, Marshal.GetFunctionPointerForDelegate(_getWindowSize));
+
+        var io = ImGui.GetIO();
+        io.ConfigFlags |= ImGuiConfigFlags.ViewportsEnable;
+        io.NativePtr->BackendPlatformName = (byte*)new FixedAsciiString("MoonWorks.SDL").DataPtr;
+        io.BackendFlags |= ImGuiBackendFlags.HasMouseCursors;
+        io.BackendFlags |= ImGuiBackendFlags.HasSetMousePos;
+        io.BackendFlags |= ImGuiBackendFlags.PlatformHasViewports;
+        io.BackendFlags |= ImGuiBackendFlags.RendererHasViewports;
+        // io.BackendFlags |= ImGuiBackendFlags.HasMouseHoveredViewport;
+
+        SDL.SDL_SetHint(SDL.SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
+
+        UpdateMonitors();
     }
 
     private static GraphicsPipeline SetupPipeline(GraphicsDevice graphicsDevice)
@@ -183,6 +251,9 @@ public class ImGuiRenderer
 
     public ImFontPtr GetFont(ImGuiFont font)
     {
+        if (IsDisposed)
+            throw new ObjectDisposedException(nameof(ImGuiRenderer));
+
         return _fonts[font];
     }
 
@@ -202,22 +273,74 @@ public class ImGuiRenderer
             throw new InvalidOperationException("Begin was called twice");
 
         _frameBegun = true;
+
         var io = ImGui.GetIO();
         io.DisplaySize = new Num.Vector2(
             _game.MainWindow.Width / _scaleFactor.X,
             _game.MainWindow.Height / _scaleFactor.Y
         );
         io.DisplayFramebufferScale = _scaleFactor;
+
+        // TODO (marpe): Check if this is needed
         var platformIo = ImGui.GetPlatformIO();
-        platformIo.Viewports[0].Pos = new Num.Vector2(0, 0);
+        SDL.SDL_GetWindowPosition(_game.MainWindow.Handle, out var x, out var y);
+        platformIo.Viewports[0].Pos = new Num.Vector2(x, y);
         platformIo.Viewports[0].Size = new Num.Vector2(_game.MainWindow.Width, _game.MainWindow.Height);
 
         io.DeltaTime = deltaTimeInSeconds;
         UpdateInput();
         UpdateMouseCursor();
-        // UpdateWindows();
-        // UpdateMonitors();
+        UpdateWindows();
+        UpdateMonitors();
         ImGui.NewFrame();
+    }
+
+    private void UpdateWindows()
+    {
+        var viewports = ImGui.GetPlatformIO().Viewports;
+        for (var i = 1; i < viewports.Size; i++)
+        {
+            var vp = viewports[i];
+            var window = WindowFromUserData(vp.PlatformUserData);
+            UpdateWindow(window, vp);
+        }
+    }
+
+    private void UpdateWindow(Window window, ImGuiViewportPtr viewport)
+    {
+        var id = SDL.SDL_GetWindowID(window.Handle);
+
+        while (SDL.SDL_PollEvent(out var evt) == 1)
+        {
+            if (evt.type != SDL.SDL_EventType.SDL_WINDOWEVENT)
+                continue;
+
+            if (id != evt.window.windowID)
+                continue;
+
+            Logger.LogInfo($"Received Event: {evt.window.type}");
+            switch (evt.window.windowEvent)
+            {
+                case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_FOCUS_GAINED:
+                    ImGui.GetIO().AddFocusEvent(true);
+                    break;
+                case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_FOCUS_LOST:
+                    ImGui.GetIO().AddFocusEvent(false);
+                    break;
+                case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESIZED:
+                    viewport.PlatformRequestResize = true;
+                    break;
+                case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_MOVED:
+                    viewport.PlatformRequestMove = true;
+                    break;
+                case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_CLOSE:
+                    viewport.PlatformRequestClose = true;
+                    break;
+                default:
+                    Logger.LogWarn($"Unhandled window event: {evt.window.windowEvent}");
+                    break;
+            }
+        }
     }
 
     private unsafe void UpdateMonitors()
@@ -297,13 +420,25 @@ public class ImGuiRenderer
             for (var i = 1; i < platformIO.Viewports.Size; i++)
             {
                 var vp = platformIO.Viewports[i];
-                // var window = (ImGuiWindow)GCHandle.FromIntPtr(vp.PlatformUserData).Target!;
-                // var windowCommandBuffer = Render(window, vp.DrawData);
-                // commandBuffers.Add(windowCommandBuffer);
+                var window = WindowFromUserData(vp.PlatformUserData);
+                var windowCommandBuffer = _game.GraphicsDevice.AcquireCommandBuffer();
+                var windowSwapchainTexture = windowCommandBuffer.AcquireSwapchainTexture(window);
+                if (windowSwapchainTexture == null)
+                {
+                    Logger.LogError("Couldn't acquire swapchain texture");
+                    continue;
+                }
+
+                Render(windowCommandBuffer, windowSwapchainTexture, vp.DrawData);
+                _game.GraphicsDevice.Submit(windowCommandBuffer);
             }
         }
     }
 
+    private static Window WindowFromUserData(IntPtr userData)
+    {
+        return (Window)(GCHandle.FromIntPtr(userData).Target ?? throw new InvalidOperationException("UserData was null"));
+    }
 
     private void Render(CommandBuffer commandBuffer, Texture swapchainTexture, ImDrawDataPtr drawData)
     {
@@ -467,8 +602,6 @@ public class ImGuiRenderer
     {
         var io = ImGui.GetIO();
 
-        UpdateMouseCursor();
-
         for (var i = 0; i < io.KeysDown.Count; i++)
         {
             if (!Enum.IsDefined((KeyCode)i))
@@ -482,8 +615,12 @@ public class ImGuiRenderer
                      _game.Inputs.Keyboard.IsDown(KeyCode.RightControl);
         io.KeyAlt = _game.Inputs.Keyboard.IsDown(KeyCode.LeftAlt) ||
                     _game.Inputs.Keyboard.IsDown(KeyCode.RightAlt);
+        io.KeySuper = _game.Inputs.Keyboard.IsDown(KeyCode.LeftMeta) ||
+                      _game.Inputs.Keyboard.IsDown(KeyCode.RightMeta);
 
-        io.MousePos = new Num.Vector2(_game.Inputs.Mouse.X, _game.Inputs.Mouse.Y);
+        SDL.SDL_GetGlobalMouseState(out var globalMouseX, out var globalMouseY);
+        io.MousePos = new Num.Vector2(globalMouseX, globalMouseY);
+        // io.MousePos = new Num.Vector2(_game.Inputs.Mouse.X, _game.Inputs.Mouse.Y);
 
         io.MouseDown[0] = _game.Inputs.Mouse.LeftButton.IsDown;
         io.MouseDown[1] = _game.Inputs.Mouse.RightButton.IsDown;
@@ -511,7 +648,6 @@ public class ImGuiRenderer
 
         _loadedTextures.Remove(textureId);
     }
-
 
     private unsafe void BuildFontAtlas()
     {
@@ -575,5 +711,138 @@ public class ImGuiRenderer
         io.Fonts.ClearTexData();
 
         io.NativePtr->FontDefault = _fonts[ImGuiFont.Default];
+    }
+
+    private void CreateWindow(ImGuiViewportPtr vp)
+    {
+        var flags = SDL.SDL_WindowFlags.SDL_WINDOW_HIDDEN | SDL.SDL_WindowFlags.SDL_WINDOW_VULKAN;
+        if ((vp.Flags & ImGuiViewportFlags.NoTaskBarIcon) != 0)
+            flags |= SDL.SDL_WindowFlags.SDL_WINDOW_SKIP_TASKBAR;
+        if ((vp.Flags & ImGuiViewportFlags.NoDecoration) != 0)
+            flags |= SDL.SDL_WindowFlags.SDL_WINDOW_BORDERLESS;
+        else
+            flags |= SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE;
+        if ((vp.Flags & ImGuiViewportFlags.TopMost) != 0)
+            flags |= SDL.SDL_WindowFlags.SDL_WINDOW_ALWAYS_ON_TOP;
+
+        var windowCreateInfo = new WindowCreateInfo
+        {
+            WindowWidth = 200,
+            WindowHeight = 200,
+            WindowTitle = "ProjectName",
+            ScreenMode = ScreenMode.Windowed,
+            SystemResizable = true
+        };
+        var ImGuiWindow = new Window(windowCreateInfo, flags);
+        if (!_game.GraphicsDevice.ClaimWindow(ImGuiWindow, windowCreateInfo.PresentMode))
+        {
+            throw new SystemException("Could not claim window!");
+        }
+
+        var gcHandle = GCHandle.Alloc(ImGuiWindow);
+        vp.PlatformUserData = (IntPtr)gcHandle;
+        Logger.LogInfo("Created window");
+    }
+
+    private void DestroyWindow(ImGuiViewportPtr vp)
+    {
+        var gcHandle = GCHandle.FromIntPtr(vp.PlatformUserData);
+        if (gcHandle.Target != null)
+        {
+            var window = (Window)gcHandle.Target;
+            window.Dispose();
+        }
+
+        vp.PlatformUserData = IntPtr.Zero;
+        gcHandle.Free();
+        Logger.LogInfo("Destroyed window");
+    }
+
+    private unsafe void GetWindowPos(ImGuiViewportPtr vp, Num.Vector2* outPos)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        SDL.SDL_GetWindowPosition(window.Handle, out var x, out var y);
+        var pos = new Num.Vector2(x, y);
+        *outPos = pos;
+    }
+
+    private void SetWindowPos(ImGuiViewportPtr vp, Num.Vector2 pos)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        SDL.SDL_SetWindowPosition(window.Handle, (int)pos.X, (int)pos.Y);
+    }
+
+    private void SetWindowSize(ImGuiViewportPtr vp, Num.Vector2 size)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        window.SetWindowSize((uint)size.X, (uint)size.Y);
+    }
+
+    private unsafe void GetWindowSize(ImGuiViewportPtr vp, Num.Vector2* outSize)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        var size = new Num.Vector2(window.Width, window.Height);
+        *outSize = size;
+    }
+
+    private void ShowWindow(ImGuiViewportPtr vp)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        SDL.SDL_ShowWindow(window.Handle);
+    }
+
+    private void SetWindowFocus(ImGuiViewportPtr vp)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        SDL.SDL_RaiseWindow(window.Handle);
+    }
+
+    private byte GetWindowFocus(ImGuiViewportPtr vp)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        var flags = (SDL.SDL_WindowFlags)SDL.SDL_GetWindowFlags(window.Handle);
+        return (flags & SDL.SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS) != 0 ? (byte)1 : (byte)0;
+    }
+
+    private byte GetWindowMinimized(ImGuiViewportPtr vp)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        var flags = (SDL.SDL_WindowFlags)SDL.SDL_GetWindowFlags(window.Handle);
+        return (flags & SDL.SDL_WindowFlags.SDL_WINDOW_MINIMIZED) != 0 ? (byte)1 : (byte)0;
+    }
+
+    private unsafe void SetWindowTitle(ImGuiViewportPtr vp, IntPtr title)
+    {
+        var window = WindowFromUserData(vp.PlatformUserData);
+        var titlePtr = (byte*)title;
+        var count = 0;
+        while (titlePtr[count] != 0)
+        {
+            titlePtr += 1;
+        }
+
+        var titleStr = Encoding.ASCII.GetString(titlePtr, count);
+        SDL.SDL_SetWindowTitle(window.Handle, titleStr);
+    }
+}
+
+public sealed class FixedAsciiString : IDisposable
+{
+    public IntPtr DataPtr { get; }
+
+    public unsafe FixedAsciiString(string s)
+    {
+        var byteCount = Encoding.ASCII.GetByteCount(s);
+        DataPtr = Marshal.AllocHGlobal(byteCount + 1);
+        fixed (char* sPtr = s)
+        {
+            var end = Encoding.ASCII.GetBytes(sPtr, s.Length, (byte*)DataPtr, byteCount);
+            ((byte*)DataPtr)[end] = 0;
+        }
+    }
+
+    public void Dispose()
+    {
+        Marshal.FreeHGlobal(DataPtr);
     }
 }
