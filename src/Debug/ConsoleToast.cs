08b29fe6176b04abff1eb51250f77c1e5c7defe5
marpe <marpe@kensei.games>
2022-12-12 10:57:02 +0100

    Add previously ignored files in src/Debug

---
+++ src/Debug/ConsoleToast.cs
@@ -0,0 +1,103 @@
+﻿namespace MyGame.Debug;
+
+public static class ConsoleToast
+{
+    private static char[] _tmp = new char[1];
+    private static int _lastCursorY;
+    private static int numLinesToDraw = 0;
+    private static float _nextRemoveTime;
+    private static float _lineDisplayDuration = 1.0f;
+    private static float _elapsedTime;
+    private static float _lineRemovePercentage = 1.0f;
+    private static float _speed = 1.0f;
+
+    private static void Update()
+    {
+        if (_lastCursorY < Shared.Console.ScreenBuffer.CursorY)
+        {
+            var numNewLines = Shared.Console.ScreenBuffer.CursorY - _lastCursorY;
+            var linesLeft = Shared.Console.ScreenBuffer.Height - numLinesToDraw;
+            var linesToAdd = Math.Min(linesLeft, numNewLines);
+            if (numLinesToDraw == 0)
+                _nextRemoveTime = _elapsedTime + linesToAdd * _lineDisplayDuration;
+            numLinesToDraw += linesToAdd;
+            if (numLinesToDraw < 0)
+            {
+                Logs.LogInfo("numLinesToDrawBug");
+            }
+        }
+
+        _lastCursorY = Shared.Console.ScreenBuffer.CursorY;
+
+        _speed = Math.Max(numLinesToDraw / 5.0f, 1.0f);
+        _elapsedTime += Shared.Game.Time.ElapsedTime * _speed;
+
+        if (numLinesToDraw > 0 && _elapsedTime >= _nextRemoveTime)
+        {
+            numLinesToDraw--;
+            _nextRemoveTime += _lineDisplayDuration;
+        }
+
+        _lineRemovePercentage = MathF.Clamp01((_nextRemoveTime - _elapsedTime) / _lineDisplayDuration);
+    }
+
+    public static void Draw(Renderer renderer, ref CommandBuffer commandBuffer, Texture renderDestination)
+    {
+        Update();
+
+        if (numLinesToDraw == 0 || Shared.Console.ScreenBuffer.CursorY == 0)
+            return;
+
+        var charSize = new Point(10, 18);
+        var displayPosition = new Vector2(0, 0);
+        displayPosition.Y = _lineRemovePercentage < 0.2f ? charSize.Y * (MathF.Map(_lineRemovePercentage, 0f, 0.2f, 0f, 1f) - 1.0f) : 0;
+
+        for (var i = 0; i < numLinesToDraw; i++)
+        {
+            var lineIndex = (Shared.Console.ScreenBuffer.CursorY - 1) - i;
+            if (lineIndex < 0)
+                break;
+
+            var lineWidth = GetLineWidth(lineIndex);
+
+            var lineRect = new Rectangle(0, (int)(displayPosition.Y + charSize.Y * (numLinesToDraw - 1 - i)), lineWidth * charSize.X, charSize.Y);
+            
+            // skip line if it's outside the render target
+            if (lineRect.Y >= renderDestination.Height)
+                continue;
+            
+            var lineAlpha = 1.0f; // i == numLinesToDraw - 1 ? _lineRemovePercentage : 1.0f;
+
+            renderer.DrawRect(lineRect, Color.Black * 0.66f * lineAlpha);
+
+            for (var j = 0; j < Shared.Console.ScreenBuffer.Width; j++)
+            {
+                Shared.Console.ScreenBuffer.GetChar(j, lineIndex, out var c, out var color);
+                if (c == '\0')
+                    break;
+                if (ConsoleScreen.CanSkipChar(c))
+                    continue;
+                var charColor = ConsoleSettings.Colors[color] * lineAlpha;
+                var position = new Vector2(lineRect.X + j * charSize.X, lineRect.Y);
+                _tmp[0] = c;
+                renderer.DrawText(FontType.ConsolasMonoMedium, _tmp, position, 0, charColor);
+            }
+        }
+
+        renderer.RunRenderPass(ref commandBuffer, renderDestination, null, null);
+    }
+
+    private static int GetLineWidth(int lineIndex)
+    {
+        var lineWidth = 0;
+        for (var j = 0; j < Shared.Console.ScreenBuffer.Width; j++)
+        {
+            Shared.Console.ScreenBuffer.GetChar(j, lineIndex, out var c, out var color);
+            if (c == '\0')
+                break;
+            lineWidth++;
+        }
+
+        return lineWidth;
+    }
+}
---
+++ src/Debug/FPSDisplay.cs
@@ -0,0 +1,86 @@
+﻿namespace MyGame.Debug;
+
+public enum FPSDisplayPosition
+{
+    TopLeft,
+    TopRight,
+    BottomRight,
+    BottomLeft
+}
+
+public class FPSDisplay
+{
+    private Stopwatch _renderStopwatch = new();
+    private Stopwatch _renderGameStopwatch = new();
+    private Stopwatch _updateStopwatch = new();
+    
+    private float _renderDurationMs;
+    private float _renderGameDurationMs;
+    private float _updateDurationMs;
+    
+    private float _peakUpdateDurationMs;
+    private float _peakRenderDurationMs;
+    private float _peakRenderGameDurationMs;
+
+    public void BeginRender()
+    {
+        _renderStopwatch.Restart();
+    }
+
+    public void EndRender()
+    {
+        _renderStopwatch.Stop();
+        _renderDurationMs = _renderStopwatch.GetElapsedMilliseconds();
+    }
+    
+    public void BeginRenderGame()
+    {
+        _renderGameStopwatch.Restart();
+    }
+
+    public void EndRenderGame()
+    {
+        _renderGameStopwatch.Stop();
+        _renderGameDurationMs = _renderGameStopwatch.GetElapsedMilliseconds();
+    }
+    
+    public void BeginUpdate()
+    {
+        _updateStopwatch.Restart();
+    }
+
+    public void EndUpdate()
+    {
+        _updateStopwatch.Stop();
+        _updateDurationMs = _updateStopwatch.GetElapsedMilliseconds();
+    }
+
+    public void DrawFPS(Renderer renderer, CommandBuffer commandBuffer, Texture renderDestination, FPSDisplayPosition corner = FPSDisplayPosition.BottomRight)
+    {
+        var origin = corner switch
+        {
+            FPSDisplayPosition.TopLeft => new Vector2(0, 0),
+            FPSDisplayPosition.TopRight => new Vector2(1, 0),
+            FPSDisplayPosition.BottomRight => new Vector2(1, 1),
+            _ => new Vector2(0, 1),
+        };
+
+        var position = renderDestination.Size() * origin;
+        _peakUpdateDurationMs = StopwatchExt.SmoothValue(_peakUpdateDurationMs, _updateDurationMs);
+        _peakRenderDurationMs = StopwatchExt.SmoothValue(_peakRenderDurationMs, _renderDurationMs);
+        _peakRenderGameDurationMs = StopwatchExt.SmoothValue(_peakRenderGameDurationMs, _renderGameDurationMs);
+
+        var updateFps = Shared.Game.Time.UpdateFps;
+        var drawFps = Shared.Game.Time.DrawFps;
+        
+        var str = $"Update: {updateFps:0.##} ({_peakUpdateDurationMs:00.00}), Draw: {drawFps:0.##} ({_peakRenderGameDurationMs:00.00}/{_peakRenderDurationMs:00.00})";
+        var strSize = renderer.TextBatcher.GetFont(FontType.ConsolasMonoMedium).MeasureString(str);
+        var min = position - strSize * origin;
+        var max = min + strSize;
+        var bg = RectangleExt.FromMinMax(min, max);
+        
+        renderer.DrawRect(bg, Color.Black * 0.66f);
+        renderer.DrawText(FontType.ConsolasMonoMedium, str, min, 0, Color.Yellow);
+        renderer.RunRenderPass(ref commandBuffer, renderDestination, null, null);
+    }
+}
